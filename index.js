b15f53947e4479e285e1afe9ef8c0b57e79e2ee6
smMazibuko <sphiwem715@gmail.com>
2025-12-31 12:21:56 +0200

    Added a listener on port 8080

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+console.log("Started!");
+
+const package = require("express");
+const PORT = 8080;
+
+const app = package();
+
+app.listen(PORT, () => console.log(`It's alive on http://localhost:${PORT}`));
